8e1a59aebe638766d3f585990783b79c8d4bf6a2
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-02-06 16:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
 // your code here
+	let arr = [0,1]
+	for(let i = 2 ; i <num;i++){
+		arr.push(arr[i-2]+arr[i-1]);
+	}
+	return(arr[num-1])
 }
 
 module.exports = fibonacci;
